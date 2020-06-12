95cab4c9df5bea52eb532d3ab644e3f07269605a
Morris Kibathi <66132909+Morris-furie@users.noreply.github.com>
2020-06-12 09:17:31 -0700

    Testing git code

---
+++ unitTesting1.py
@@ -0,0 +1,29 @@
+#unite test case for three stirng methods
+import unittest #importing class library
+
+#creating a class
+#this is where the unit test goes for testing the function
+#passing the class library
+class TestStringMethods(unittest.TestCase): 
+
+	def test_upper(self):
+		self.assertEqual('foo'.upper(), 'FOO')
+
+	def test_isupper(self):
+		#this is what is going to be tested
+		sefl.assertTrue('FOO'.isupper())
+		self.assertFalse('Foo'.isupper())
+
+	def test_split(self):
+
+		#create a string variable
+		s = 'hello world'
+		self.assertEqual(s.split(), ['hello', 'world'])
+
+		#chech tht s.split fail when the seperator is not a string
+
+		with self.assertRaises(TypeError):
+			s.split(2)
+
+if __name__ == '__main__':
+	unittest.main()
